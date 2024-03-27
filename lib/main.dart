160ccad4316e40bf938e0900a6ff7a4e1d8ac9f0
admin <william0129@gmail.com>
2024-03-27 13:05:10 +0800

    add showModalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/widgets/expenses.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: 
+    const MaterialApp(
+      home: Expenses(),
     ),
   ); 
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
-import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses ({super.key});
@@ -30,10 +30,26 @@ class _ExpensesState extends State<Expenses> {
     ),
 
   ];
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(
+      context: context, 
+      builder: (ctx) => const NewExpense(),
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add)
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
