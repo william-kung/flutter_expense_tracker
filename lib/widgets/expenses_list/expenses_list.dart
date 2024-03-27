17c19c2c244d0da60d843ae361ad3b632103765f
admin <william0129@gmail.com>
2024-03-27 18:03:55 +0800

    create full screen modal

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,14 +3,26 @@ import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget{
-  const ExpensesList ({super.key, required this.expenses,});
+  const ExpensesList ({
+    super.key, 
+    required this.expenses,
+    // required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  // final void Function (Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length, 
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),);
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        // onDismissed: (direction){
+        //   onRemoveExpense(expenses[index]);
+        // },
+        child: ExpenseItem(expenses[index]),
+      ),
+    );
   }
 }
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -63,6 +63,7 @@ class _NewExpenseState extends State<NewExpense> {
         date: _selectedDate!,
       )
     );
+    Navigator.pop(context);
   }
 
 
@@ -77,7 +78,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
